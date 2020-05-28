87711adc613d1a5f8aad72f9fa35d1e1245824f8
Nanomicin <60762895+Nanomicin@users.noreply.github.com>
2020-05-28 21:15:33 +0700

    Add files via upload

---
+++ FinalProject/tentang.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang</title>
+    <?php include "style.php"; ?>
+    <link rel="stylesheet" href="css/tentang.css">
+</head>
+
+<body>
+    <div id="main-content">
+        <?php include 'header-sidebar.php';?>
+        <div id="isi">
+            <h1>Tentang Kami</h1>
+            <h3>Sejarah</h3>
+            <p>Rhodes Island Pharmaceutical, Inc., yang dapat disebut Rhodes Island atau Rhodes merupakan
+              sebuah perusahaan farmasi yang fokus pada infeksi ‘originium’. Namun perusahaan ini juga turut
+              ambil bagian dalam menanggulangi efek sosial negatif yang timbul dari infeksi ‘originium tersebut,
+              hal ini dilakukan dengan cara melakukan pemeriksaan gratis pada pasien yang terinfeksi ‘originium’,
+              menyebarkan literatur terkait infeksi ‘originium’, dan melakukan pengaman terhadap pasien terinfeksi
+              ‘originium’ yang melakukan kerusuhan. Rhodes island memiliki pusat pada Rhodes island Infrastructure Complex
+              atau R.I.I.C yang merupakan kerjasama antara Rhodes Island Pharmaceutical, Inc dan RIM Biliton. Rhodes Island di
+              pimpin oleh Amiya, dengen ‘Doctor’ sebagai penasihatnya.</p>
+            <h3>Visi & Misi</h3>
+            <p>Rhodes Island memiliki motto “May I enjoy my life and practice my art / Respected by all men
+               and in all times” yang merupakan kutipan dari sumpah hipokratis, dimana sumpah hipokratis tersebut
+               adalah sumpah yang mengikat dokter medis dengan profesi dan tanggung jawabnya. Visi dari Rhodes island
+                adalah membangun masa depan baru bagi orang-orang yang terinfeksi. Misi dari Rhodes island adalah  memberikan
+                perawatan dan perlindungan bagi orang-orang yang terinfeksi, menyelesaikan krisis ‘oriphaty’</p>
+        </div>
+        <div id="clear"></div>
+    </div>
+</body>
+
+</html>
